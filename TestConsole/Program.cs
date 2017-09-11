39013695d551aa71a9c9e2e5d6eca0ab1daba1a8
LDS2580 <LDS2580@LDS-PC>
2017-09-11 17:09:37 +0900

    .

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -92,10 +92,7 @@ namespace TestConsole
             Console.ReadLine();
 
             //------------
-            //------------
-            //------------
-            //------------
-            //------------
+            //------------aaa
         }
         public static string PointConvert(float num, int point_length)
         {
